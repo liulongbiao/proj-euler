50258e89baadbda04e112675360f32d0da42f5b6
LiuLongbiao <liulongbiao@gmail.com>
2013-04-23 17:39:17 +0800

    resolve problem 058

--- src/proj_euler/core.clj
+++ src/proj_euler/core.clj
@@ -198,6 +198,29 @@
                                   [n i])]
     (first consecutive-primes-sum)))
 
+(defn spiral-layer [n]
+  (let [right-down (expt (inc (* 2 n)) 2)]
+    (map #(- right-down (* 2 n %)) (range 3 -1 -1))))
+
+(defn above? [[n pt]] (> (/ pt (inc (* 4 n))) 0.1))
+
+(defn p058
+  ([] (p058 0.1))
+  ([percent]
+    (letfn [(spiral-layer [n]
+              (let [right-down (expt (inc (* 2 n)) 2)]
+                (map #(- right-down (* 2 n %)) (range 3 -1 -1))))
+            (layer [n] [n (count (filter prime? (spiral-layer n)))])
+            (red [[_ pt] [n p]] [n (+ pt p)])
+            (above? [v] (> (/ (v 1) (inc (* 4 (v 0)))) percent))
+            (side-length [[n pt]] (inc (* 2 n)))]
+      (->> (iterate inc 1)
+        (map layer)
+        (reductions red)
+        (drop-while above?)
+        first
+        side-length))))
+
 (defn p067 []
   (with-open [rdr (reader (resource "proj_euler/triangle.txt"))]
     (let [triangle (for [row (line-seq rdr)]
